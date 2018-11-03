aac0b32eaae9c6d376f148b248241022cbdcedad
DYroot <dileepayasas@gmail.com>
2018-11-04 01:08:30 +0530

    python print command

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-test
- school
 
+#!/usr/bin/env python
+
+# import modules used here -- sys is a very standard one 
+import sys
+# gather our code in a main ()  function 
+def main():
+   print 'hello there', sys.argv[1]
+
+# the program.
+if  __name__ == '__main__':
+  main()
